6e168f80d3a39397db15d278b6178bbc1dc66d26
Ali <106279062+AliiRad@users.noreply.github.com>
2023-09-20 17:50:16 +0330

    init Mongodb-java

---
+++ Mogodb-java/src/main/java/com/home/Mogodbjava/MogodbJavaApplication.java
@@ -0,0 +1,13 @@
+package com.home.Mogodbjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MogodbJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MogodbJavaApplication.class, args);
+	}
+
+}
---
+++ Mogodb-java/src/test/java/com/home/Mogodbjava/MogodbJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.Mogodbjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MogodbJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
